45ac9c10bcbcf564484ec4dffb58d5ebf0cbb101
GuilhermeLamim <guilherme.lamim@ambevtech.com.br>
2021-03-26 00:27:38 -0300

    using Observable with simple bd

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../product.service.ts.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -8,16 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name:'',
+    price: null
+  }
   constructor(private productService: ProductService,
     private router: Router) { }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void { }
 
   createProduct(): void {
-    this.productService.showMensage("Operação executada com sucesso");
+    console.log('CHEGOU!!!2')
+    this.productService.create(this.product).subscribe(() => {
+      console.log('CHEGOU!!!')
+      this.productService.showMensage("Operação executada com sucesso");
+      this.router.navigate(['/products']);
+    })
   }
+
   cancel(): void {
     this.router.navigate(['/products']);
   }
--- src/app/components/product/product.service.ts.service.ts
+++ src/app/components/product/product.service.ts.service.ts
@@ -1,12 +1,20 @@
+import { Product } from './product-create/product.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  baseUrl = "http://localhost:3001/products"
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private http: HttpClient
+    ) { }
 
   showMensage(msg: string): void {
     this.snackBar.open(msg, 'undo', {
@@ -14,5 +22,9 @@ export class ProductService {
       horizontalPosition: "right",
       verticalPosition: "top"
     })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
   }
 }
